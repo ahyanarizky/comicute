266d619c3590c73022a16d983d86446582c2ff7b
Septian A Tama <sfirst.tama@gmail.com>
2016-11-04 14:41:24 +0700

    deleting path delete for delete route and create for add new user route

--- server/routes/apiUser.js
+++ server/routes/apiUser.js
@@ -4,8 +4,8 @@ const controller = require('../controller/controller');
 
 
 router.get('/', controller.getUser) //get all users
-router.post('/create', controller.addUser) //add user
-router.delete('/delete/:id', controller.deleteUser) //delete user with id
+router.post('/', controller.addUser) //add user
+router.delete('/:id', controller.deleteUser) //delete user with id
 router.get('/:id', controller.getUserById) //get user data for edit
 router.put('/:id', controller.updateUser) //update user
 
