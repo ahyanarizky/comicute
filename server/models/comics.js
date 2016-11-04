35030b99889eeb506bd44da4df58be0435cfed63
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-04 11:46:49 +0700

    create comics model

---
+++ server/models/comics.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+var Comic = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    photoPath: {
+        type: String,
+        required: true
+    },
+    filePath: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Comic', Comic)
