56825d20bfb330f3b78b019f50174af84feeaf53
Septian A Tama <sfirst.tama@gmail.com>
2016-11-04 15:30:44 +0700

    change name from controller.js to userController.js, moving the file controller part to fileCOntroller.js

---
+++ server/controller/userController.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/users')
+const File = require('../models/file')
+
+module.exports = {
+
+//    get user
+    getUser: (req, res) => {
+        User.find({}, (err, data) => {
+            if (err) res.json(err)
+            else res.json(data)
+        })
+    },
+
+    //add user
+    addUser: (req, res) => {
+        const user = {
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            birthdate: req.body.birthdate,
+            role: req.body.role
+        }
+
+        User.create(user, (err, data) => {
+            if (err) res.json(err)
+            else res.json(data)
+        })
+    },
+
+//    delete user
+    deleteUser: (req, res) => {
+        User.findOneAndRemove({
+            _id: req.params.id
+        }, (err, data) => {
+            if (err) res.json(err)
+            else res.json(data)
+        })
+    },
+
+//    get user data for edit
+    getUserById: (req, res) => {
+        User.findOne({
+            _id: req.params.id
+        }, (err, data) => {
+            if (err) res.json(err)
+            else res.json(data)
+        })
+    },
+
+    //edit user
+    updateUser: (req, res) => {
+        User.findOneAndUpdate({
+            _id: req.params.id
+        }, {
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            birthdate: req.body.birthdate,
+            role: req.body.role
+        }, {
+            new: true,
+            upsert: true
+        }, (err, data) => {
+            if (err) res.json(err)
+            else res.json(data)
+        })
+    }
+}
