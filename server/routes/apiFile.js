19801328c05dbbf897987b92d3a55f13be713171
Septian A Tama <sfirst.tama@gmail.com>
2016-11-04 15:22:07 +0700

    renaming apiComic to apiFile

---
+++ server/routes/apiFile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/controller');
+
+router.get('', controller.getFiles) //get all files
+router.post('/', controller.addFile) //add file
+router.delete('/:id', controller.deleteFile) //delete file with id
+router.get('/:id', controller.getFileById) //get file data for edit
+router.put('/:id', controller.updateFile) //update file
+
+module.exports = router;
