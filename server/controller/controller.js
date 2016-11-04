1bde75629ee75ed8699b45820aa1523862754bf7
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-04 11:54:15 +0700

    create controller crud structure for users and comic in conctroller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,1 +1,60 @@
-module.exports = {}
+const express = require('express')
+const router = express.Router()
+const User = require('../models/users')
+
+module.exports = {
+
+//    USERS MODEL
+//    get user
+    getUser: function () {
+
+    },
+
+    //add user
+    addUser: function () {
+
+    },
+
+//    delete user
+    deleteUser: function () {
+
+    },
+
+//    get user data for edit
+    getEditUser: function () {
+
+    },
+
+    //edit user
+    editUser: function () {
+
+    },
+
+//    COMICS MODELS
+
+//    get comic
+    getComic: function () {
+
+    },
+
+//    add comic
+    addComic: function () {
+
+    },
+
+//    delete comic
+    deleteComic: function () {
+
+    },
+
+//    get comic data for edit
+    getEditComic: function () {
+
+    },
+
+//    edit comic
+    editComic: function () {
+
+    }
+
+}
