0e5aa2e7d7ff812b0ae3e39aa38e86194ca0ed69
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-04 12:09:06 +0700

    create crud structure in routes for users and comics

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/controller');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+
+router.get('/user') //get all users
+router.post('/user/create') //add user
+router.delete('/user/:id') //delete user with id
+router.get('/user/:id') //get user data for edit
+router.put('/user/update') //update user
+
+router.get('/comic') //get all comics
+router.post('/comic/create') //add comic
+router.delete('/comic/:id') //delete comic with id
+router.get('/comic/:id') //get comic data for edit
+router.put('/comic/update') //update comic
+
 
 module.exports = router;
