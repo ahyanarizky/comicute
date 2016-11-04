da7613cf9fdadc9cac3a19cfd9b04deee7ac4aa1
Septian A Tama <sfirst.tama@gmail.com>
2016-11-04 14:39:38 +0700

    modifiying routes for api user, make it simple

--- server/routes/apiUser.js
+++ server/routes/apiUser.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const controller = require('../controller/controller');
 
 
-router.get('/user', controller.getUser) //get all users
-router.post('/user/create', controller.addUser) //add user
-router.delete('/user/delete/:id', controller.deleteUser) //delete user with id
-router.get('/user/:id', controller.getUserById) //get user data for edit
-router.put('/user/update/:id', controller.updateUser) //update user
+router.get('/', controller.getUser) //get all users
+router.post('/create', controller.addUser) //add user
+router.delete('/delete/:id', controller.deleteUser) //delete user with id
+router.get('/:id', controller.getUserById) //get user data for edit
+router.put('/:id', controller.updateUser) //update user
 
 module.exports = router;
